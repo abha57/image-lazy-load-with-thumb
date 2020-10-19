952d62085a9d33dc9070906ed9171e40ddbde0f5
abha57 <apaar256@gmail.com>
2020-10-19 22:13:39 +0530

    lazy loaded

--- src/components/ImageContainer/index.jsx
+++ src/components/ImageContainer/index.jsx
@@ -6,18 +6,26 @@ const ImageContainer = (props) => {
     if (!("loading" in HTMLImageElement.prototype)) {
       return true;
     }
-    return false;
+    return true; //false
   };
 
   const { images } = props;
   const [observer, setIntersectionObserver] = useState(null);
-  const [refs, setRefs] = useState([]);
-  const [isPolyfillNeeded] = useState(initPolyfill);
+  // const [isPolyfillNeeded] = useState(initPolyfill);
   const [loadedImages, setLoadedImages] = useState([]);
   const [highRefs, setHighRefs] = useState([]);
+  const [thumbRefs, setThumbRefs] = useState([]); // isPolyfillNeeded
 
   useEffect(() => {
     setHighRefs((newRefs) =>
+      new Array(images.length)
+        .fill()
+        .map((_, index) => newRefs[index] || createRef())
+    );
+  }, [images]);
+
+  useEffect(() => {
+      setThumbRefs((newRefs) =>
       new Array(images.length)
         .fill()
         .map((_, index) => newRefs[index] || createRef())
@@ -34,36 +42,33 @@ const ImageContainer = (props) => {
   }, [images]);
 
   useEffect(() => {
-    if (isPolyfillNeeded) {
       const observer = new IntersectionObserver((entries, self) => {
         entries.map((entry) => {
           if (entry.isIntersecting) {
-            entry.target.src = entry.target.dataSet.thumb;
+            entry.target.src = entry.target.dataset.highres || entry.target.dataset.thumb;
             self.unobserve(entry.target);
           }
         });
       }, {});
       setIntersectionObserver(observer);
-    }
-  }, [isPolyfillNeeded]);
+    
+  }, []);
 
   useEffect(() => {
-    if (isPolyfillNeeded && images.length > 0) {
-      setRefs((newRef) =>
-        new Array(images.length)
-          .fill()
-          .map((_, index) => newRef[index] || createRef())
-      );
-    }
-  }, [isPolyfillNeeded, images]);
-
-  useEffect(() => {
-    if (isPolyfillNeeded && images.length > 0) {
-      images.map((image) => {
-        observer.observe(image);
+    if (thumbRefs.length > 0) {
+      thumbRefs.map((ref) => {
+        observer.observe(ref.current);
       });
     }
-  }, [images, observer, isPolyfillNeeded]);
+  }, [thumbRefs, observer]);
+
+  useEffect(() => {
+    if (highRefs.length > 0) {
+      highRefs.map((ref) => {
+        observer.observe(ref.current);
+      });
+    }
+  }, [highRefs, observer]);
 
   const setVisiblilty = (ref, index) => () => {
     if (ref && ref.current) {
@@ -78,13 +83,8 @@ const ImageContainer = (props) => {
 
   return (
     <>
-      {/* {isPolyfillNeeded && (
-    <div className='images-container'>
-      <image />
-    </div>
-  )} */}
-      {!isPolyfillNeeded &&
-        images.map((image, index) => (
+      <div className='images-container'>
+      {images.map((image, index) => (
           <div className="image-container">
             <div className="aspect-ratio-box" key={image.alt}>
               <img
@@ -92,23 +92,30 @@ const ImageContainer = (props) => {
                 className="high-res"
                 key={`${image.alt}-highRes-${index}`}
                 alt={image.alt}
-                loading="lazy"
-                src={image.highResSrc}
                 onLoad={setVisiblilty(highRefs[index], index)}
+                data-highres={image.highResSrc}
               />
-
               {loadedImages && loadedImages[index] && !loadedImages[index].loaded && (
                 <img
+                  ref={thumbRefs[index]}
                   className="thumb-image"
                   key={`${image.alt}-thumb`}
                   alt={image.alt}
-                  loading="lazy"
-                  src={image.thumbSrc}
+                  data-thumb={image.thumbSrc}
                 />
                )}
             </div>
+            <div className='static'>
+              This section is to check for the layout shifts when images load.
+            </div>
           </div>
-        ))}
+        ))
+
+      }
+
+      
+      
+    </div>
     </>
   );
 };
